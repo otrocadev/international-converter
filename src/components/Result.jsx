c243c9c819abf70386753737c71d775261e5f97a
Ot Roca <otrocadev@gmail.com>
2025-05-14 12:37:24 +0200

    Add formatting function to have a better readability on the result number

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,18 @@
 function Result({ convertedNumber, unit = '' }) {
+  const formatNumber = (number) => {
+    let newNumber = number
+    if (newNumber > 1000000) return newNumber.toExponential(4)
+    if (newNumber % 1 !== 0) return newNumber.toFixed(3)
+    return newNumber
+  }
+
   return (
     <div>
       {convertedNumber ? (
         <section className="flex flex-col items-start w-full">
           <label>Result:</label>
           <p id="responseNumber" className="text-4xl font-exodos">
-            {convertedNumber} {unit}
+            {formatNumber(convertedNumber)} {unit}
           </p>
         </section>
       ) : null}
